551f7878f77f715c05e71321f14a56f24c5084eb
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2024-05-01 15:52:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -42,10 +42,10 @@ async def download_file(event, file_name, file_url, progress_message):
             else:
                 video_file_extension = '.mp4'
                 downloaded_video_path = f"{video_download_directory}/{file_name}{video_file_extension}"
-                command_to_exec = ["yt-dlp", "--geo-bypass-country", "US", "--retries", "25", "--fragment-retries", "25", "--force-overwrites", "--no-keep-video", "-i", "--external-downloader", "axel", "--external-downloader-args", "axel:-n 10 -a", "--add-metadata", "-o", downloaded_video_path, file_url]
+                command_to_exec = ["yt-dlp", "--geo-bypass-country", "US", "--retries", "25", "--fragment-retries", "25", "--force-overwrites", "--no-keep-video", "-i", "--external-downloader", "axel", "--external-downloader-args", "axel:-n 5 -a", "--add-metadata", "-o", downloaded_video_path, file_url]
                 subprocess.run(command_to_exec, check=True)
                 thumb_image_path = f"{thumbnail_download_directory}/{file_name}.jpg"
-                thumb_cmd = f'ffmpeg -hide_banner -loglevel quiet -i {downloaded_video_path} -ss 00:00:02 -vframes 1 -update 1 {thumb_image_path}'
+                thumb_cmd = f'ffmpeg -hide_banner -loglevel quiet -i {downloaded_video_path} -ss 00:00:01 -vframes 1 -update 1 {thumb_image_path}'
                 os.system(thumb_cmd)
                 vid = cv2.VideoCapture(downloaded_video_path)
                 width = vid.get(cv2.CAP_PROP_FRAME_WIDTH)
