747060f6103baa2adbd941899f7caf142775b5db
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2024-05-01 13:39:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import os, subprocess, glob
+import os, subprocess
 from telethon import TelegramClient, events
 from dotenv import load_dotenv
 import cv2
@@ -39,28 +39,26 @@ async def handle_docs(event):
                     await progress_message.edit(f"Uploading {pdf_file_name}...")
                     await telethon_client.send_file(event.chat_id, file=downloaded_pdf_path, caption=pdf_file_name)
                 else:
-                    command_to_exec = ["yt-dlp", "--geo-bypass-country", "US", "--retries", "25", "--fragment-retries", "25", "--force-overwrites", "--no-keep-video", "-i", "--external-downloader", "axel", "--external-downloader-args", "axel:-n 10 -a", "--add-metadata", "-o", f"{video_download_directory}/{file_name}", file_url]
+                    video_file_extension = '.mp4'
+                    downloaded_video_path = f"{video_download_directory}/{file_name}{video_file_extension}"
+                    command_to_exec = ["yt-dlp", "--geo-bypass-country", "US", "--retries", "25", "--fragment-retries", "25", "--force-overwrites", "--no-keep-video", "-i", "--external-downloader", "axel", "--external-downloader-args", "axel:-n 5 -a", "--add-metadata", "-o", downloaded_video_path, file_url]
                     subprocess.run(command_to_exec, check=True)
-                    video_file_pattern = f"{video_download_directory}/{file_name}.*"
-                    video_file_list = glob.glob(video_file_pattern)
-                    if video_file_list:
-                        downloaded_video_path = video_file_list[0]
-                        thumb_image_path = f"{thumbnail_download_directory}/{file_name}.jpg"
-                        thumb_cmd = f'ffmpeg -hide_banner -loglevel quiet -i {downloaded_video_path} -ss 00:00:02 -vframes 1 -update 1 {thumb_image_path}'
-                        os.system(thumb_cmd)
-                        vid = cv2.VideoCapture(downloaded_video_path)
-                        width = vid.get(cv2.CAP_PROP_FRAME_WIDTH)
-                        height = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
-                        duration = int(vid.get(cv2.CAP_PROP_FRAME_COUNT) / vid.get(cv2.CAP_PROP_FPS))
-                        vid.release()
-                        attributes = [DocumentAttributeVideo(
-                            w=int(width),
-                            h=int(height),
-                            duration=duration,
-                            supports_streaming=True
-                        )]
-                        await progress_message.edit(f"Uploading {file_name}...")
-                        await telethon_client.send_file(event.chat_id, file=downloaded_video_path, thumb=thumb_image_path, attributes=attributes)
+                    thumb_image_path = f"{thumbnail_download_directory}/{file_name}.jpg"
+                    thumb_cmd = f'ffmpeg -hide_banner -loglevel quiet -i {downloaded_video_path} -ss 00:00:02 -vframes 1 -update 1 {thumb_image_path}'
+                    os.system(thumb_cmd)
+                    vid = cv2.VideoCapture(downloaded_video_path)
+                    width = vid.get(cv2.CAP_PROP_FRAME_WIDTH)
+                    height = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
+                    duration = int(vid.get(cv2.CAP_PROP_FRAME_COUNT) / vid.get(cv2.CAP_PROP_FPS))
+                    vid.release()
+                    attributes = [DocumentAttributeVideo(
+                        w=int(width),
+                        h=int(height),
+                        duration=duration,
+                        supports_streaming=True
+                    )]
+                    await progress_message.edit(f"Uploading {file_name}...")
+                    await telethon_client.send_file(event.chat_id, file=downloaded_video_path, thumb=thumb_image_path, attributes=attributes)
             except Exception as e:
                 await event.respond(f"Failed to download {file_name}. Error: {str(e)}")
                 continue
@@ -76,3 +74,5 @@ print("Bot successfully deployed.")
             
 telethon_client.run_until_disconnected()
 
+
+        
