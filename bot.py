8b4f3c9c3c85a2cbd813fc84f8fb494dbc56d10e
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2024-05-01 15:55:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,6 +24,9 @@ telethon_client.start(bot_token=os.getenv("BOT_TOKEN"))
 
 semaphore = asyncio.Semaphore(3)
 upload_queue = asyncio.Queue()
+downloaded_files = 0
+uploaded_files = 0
+failed_files = 0
 
 @telethon_client.on(events.NewMessage(pattern='/start'))
 async def start(event):
@@ -31,14 +34,14 @@ async def start(event):
 
 async def download_file(event, file_name, file_url, progress_message):
     async with semaphore:
+        global downloaded_files, failed_files
         try:
-            progress_update = await progress_message.edit(f"Downloading {file_name}...")
             if file_url.endswith('.pdf'):
                 pdf_file_name = f"{file_name}.pdf"
                 command_to_exec = ["yt-dlp", "-o", f"{pdf_download_directory}/{pdf_file_name}", file_url]
                 subprocess.run(command_to_exec, check=True)
                 downloaded_pdf_path = f"{pdf_download_directory}/{pdf_file_name}"
-                await upload_queue.put((event, downloaded_pdf_path, pdf_file_name, None, None, progress_update.id))
+                await upload_queue.put((event, downloaded_pdf_path, pdf_file_name, None, None))
             else:
                 video_file_extension = '.mp4'
                 downloaded_video_path = f"{video_download_directory}/{file_name}{video_file_extension}"
@@ -58,40 +61,44 @@ async def download_file(event, file_name, file_url, progress_message):
                     duration=duration,
                     supports_streaming=True
                 )]
-                await upload_queue.put((event, downloaded_video_path, file_name, thumb_image_path, attributes, progress_update.id))
+                await upload_queue.put((event, downloaded_video_path, file_name, thumb_image_path, attributes))
+            downloaded_files += 1
         except Exception as e:
-            await event.respond(f"Failed to download {file_name}. Error: {str(e)}")
+            failed_files += 1
+        finally:
+            await progress_message.edit(f"Downloaded {downloaded_files}/{len(lines)} files, Uploaded {uploaded_files}/{len(lines)} files, Failed {failed_files} files")
 
-async def upload_file():
+async def upload_file(progress_message):
+    global uploaded_files, failed_files
     while True:
-        event, file_path, file_name, thumb_image_path, attributes, progress_message_id = await upload_queue.get()
+        event, file_path, file_name, thumb_image_path, attributes = await upload_queue.get()
         try:
-          #  await event.respond(f"Uploading {file_name}...")
             if thumb_image_path and attributes:
                 await telethon_client.send_file(event.chat_id, file=file_path, thumb=thumb_image_path, attributes=attributes, caption=file_name)
                 os.remove(thumb_image_path)
             else:
                 await telethon_client.send_file(event.chat_id, file=file_path, caption=file_name)
             os.remove(file_path)
-            await telethon_client.delete_messages(event.chat_id, [progress_message_id])
+            uploaded_files += 1
         except Exception as e:
-            await event.respond(f"Failed to upload {file_name}. Error: {str(e)}")
-        upload_queue.task_done()
+            failed_files += 1
+        finally:
+            await progress_message.edit(f"Downloaded {downloaded_files}/{len(lines)} files, Uploaded {uploaded_files}/{len(lines)} files, Failed {failed_files} files")
+            upload_queue.task_done()
 
 @telethon_client.on(events.NewMessage(incoming=True, pattern=None))
 async def handle_docs(event):
     if event.document:
-        progress_message = await event.respond("Preparing to download...")
+        progress_message = await event.respond("Starting the process...")
         file_path = await event.download_media(file=download_directory)
         with open(file_path, 'r') as file:
             lines = file.readlines()
-        download_tasks = [download_file(event, line.strip().split(':', 1)[0], line.strip().split(':', 1)[1], progress_message) for line in lines]
-        upload_task = asyncio.create_task(upload_file())
+        download_tasks = [download_file(event, line.strip().split(':', 1)[0], line.strip().plit(':', 1)[1], progress_message) for line in lines]
+        upload_task = asyncio.create_task(upload_file(progress_message))
         await asyncio.gather(*download_tasks)
         await upload_queue.join()
         upload_task.cancel()
         os.remove(file_path)
-        await telethon_client.delete_messages(event.chat_id, [progress_message.id])
 
 print("SUCESSFULLY DEPLOYED")
 telethon_client.run_until_disconnected()
