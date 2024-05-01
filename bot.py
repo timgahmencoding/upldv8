5cc25c73fe6952f32b77ef34963ce509435fdebc
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2024-05-01 11:33:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,19 @@
-import os
-import subprocess
+import os, subprocess, glob
 from moviepy.editor import VideoFileClip
-from telethon import TelegramClient, events, sync
-from telethon.tl.types import DocumentAttributeVideo
+from telethon import TelegramClient, events
 from dotenv import load_dotenv
 import pyfiglet
-from FastTelethon import download_file, upload_file
+from FastTelethon import upload_file
 
 load_dotenv()
-
 download_directory = "./downloads"
 pdf_download_directory = f"{download_directory}/pdfs"
 video_download_directory = f"{download_directory}/video"
 thumbnail_download_directory = f"{download_directory}/thumbnail"
-
-if not os.path.exists(download_directory):
-    os.makedirs(download_directory)
-    os.makedirs(pdf_download_directory)
-    os.makedirs(video_download_directory)
-    os.makedirs(thumbnail_download_directory)
-
+os.makedirs(download_directory, exist_ok=True)
+os.makedirs(pdf_download_directory, exist_ok=True)
+os.makedirs(video_download_directory, exist_ok=True)
+os.makedirs(thumbnail_download_directory, exist_ok=True)
 telethon_client = TelegramClient('BULK-UPLOAD-BOT', int(os.getenv("API_ID")), os.getenv("API_HASH"))
 telethon_client.start(bot_token=os.getenv("BOT_TOKEN"))
 
@@ -39,59 +33,41 @@ async def handle_docs(event):
             try:
                 await progress_message.edit(f"Downloading {file_name}...")
                 if file_url.endswith('.pdf'):
-                    command_to_exec = [
-                        "yt-dlp",
-                        "-o", f"{pdf_download_directory}/{file_name}.pdf",              
-                        file_url
-                    ]
+                    pdf_file_name = f"{file_name}.pdf"
+                    command_to_exec = ["yt-dlp", "-o", f"{pdf_download_directory}/{pdf_file_name}", file_url]
                     subprocess.run(command_to_exec, check=True)
-                    downloaded_file_path = f"{pdf_download_directory}/{file_name}"
-                    await progress_message.edit(f"Uploading {file_name}...")
-                    with open(downloaded_file_path, 'rb') as upload_file_object:
+                    downloaded_pdf_path = f"{pdf_download_directory}/{pdf_file_name}"
+                    await progress_message.edit(f"Uploading {pdf_file_name}...")
+                    with open(downloaded_pdf_path, 'rb') as upload_file_object:
                         uploader = await upload_file(telethon_client, upload_file_object, event.chat_id)
-                        await telethon_client.send_file(event.chat_id, uploader, caption=file_name)
+                        await telethon_client.send_file(event.chat_id, uploader, caption=pdf_file_name)
                 else:
-                    command_to_exec = [
-                        "yt-dlp",
-                        "--geo-bypass-country", "US",
-                        "--retries",
-                        "25",
-                        "--fragment-retries",
-                        "25",
-                        "--force-overwrites",
-                        "--no-keep-video",
-                        "-i",
-                        "--external-downloader", "axel",
-                        "--external-downloader-args", "'-n 5 -a -k 1M -s 16'"
-                        "--add-metadata",
-                        "-o", f"{video_download_directory}/{file_name}",
-                        file_url
-                    ]
+                    command_to_exec = ["yt-dlp", "--geo-bypass-country", "US", "--retries", "25", "--fragment-retries", "25", "--force-overwrites", "--no-keep-video", "-i", "--external-downloader", "axel", "--external-downloader-args", "-n 5 -a -k 1M -s 16", "--add-metadata", "-o", f"{video_download_directory}/{file_name}", file_url]
                     subprocess.run(command_to_exec, check=True)
-                    downloaded_file_path = f"{video_download_directory}/{file_name}.mp4"
-                    thumbnail_path = f"{thumbnail_download_directory}/{file_name}.jpg"
-                    clip = VideoFileClip(downloaded_file_path + '.mp4')
-                    clip.save_frame(thumbnail_path, t=1)
-                    width, height = clip.size
-                    duration = int(clip.duration)
-                    clip.close()
-                    await progress_message.edit(f"Uploading {file_name}...")
-                    with open(downloaded_file_path, 'rb') as upload_file_object:
-                        uploader = await upload_file(
-                            client=telethon_client,
-                            file=upload_file_object,
-                            name=file_name
-                        )
-                        await telethon_client.send_file(event.chat_id, uploader, caption=file_name)
+                    video_file_pattern = f"{video_download_directory}/{file_name}.*"
+                    video_file_list = glob.glob(video_file_pattern)
+                    if video_file_list:
+                        downloaded_video_path = video_file_list[0]
+                        thumbnail_path = f"{thumbnail_download_directory}/{file_name}.jpg"
+                        clip = VideoFileClip(downloaded_video_path)
+                        clip.save_frame(thumbnail_path, t=1)
+                        clip.close()
+                        await progress_message.edit(f"Uploading {file_name}...")
+                        with open(downloaded_video_path, 'rb') as upload_file_object:
+                            uploader = await upload_file(telethon_client, upload_file_object, event.chat_id)
+                            await telethon_client.send_file(event.chat_id, uploader, caption=file_name)
             except Exception as e:
                 await event.respond(f"Failed to download {file_name}. Error: {str(e)}")
                 continue
         os.remove(file_path)
-        if os.path.exists(downloaded_file_path):
-            os.remove(downloaded_file_path)
+        if os.path.exists(downloaded_pdf_path):
+            os.remove(downloaded_pdf_path)
+        if os.path.exists(downloaded_video_path):
+            os.remove(downloaded_video_path)
+        if os.path.exists(thumbnail_path):
             os.remove(thumbnail_path)
 
 custom_fig = pyfiglet.Figlet(font='small')
 print(custom_fig.renderText('Bot deployed'))
-
 telethon_client.run_until_disconnected()
+        
